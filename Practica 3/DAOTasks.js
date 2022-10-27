784e8d15e8026284f41117ddc66e97c5dbdfa478
SaMuelete01 <105879110+dr4g0nWT@users.noreply.github.com>
2022-10-27 17:09:12 +0200

    mysql module

--- Practica 3/DAOTasks.js
+++ Practica 3/DAOTasks.js
@@ -2,33 +2,22 @@
 
 class DAOTasks {
     constructor(pool) {
-        this._pool = pool;
+        this.pool = pool;
     }
 
-    isUserCorrect(email, password, callback) {
-        this.pool.getConnection(function (err, connection) {
-            if (err) {
-                callback(new Error("Error de conexión a la base de datos"));
+    getAllTasks(email, callback){
+        this.pool.getConnection(function(err, connection){
+            if(err){
+                callback(new Error("Error de conexión a la base de datos"))
             }
-            else {
-                connection.query("SELECT * FROM user WHERE email = ? AND password = ?",
-                    [email, password],
-                    function (err, rows) {
-                        connection.release(); // devolver al pool la conexión
-                        if (err) {
-                            callback(new Error("Error de acceso a la base de datos"));
-                        }
-                        else {
-                            if (rows.length === 0) {
-                                callback(null, false); //no está el usuario con el password proporcionado
-                            }
-                            else {
-                                callback(null, true);
-                            }
-                        }
-                    });
+            else{
+                connection.query("SELECT idUser FROM aw_tareas_usuarios WHERE email = ?" , [email], 
+                function(err, rows){
+                    if(err){
+                        callback(new Error("Error de acceso a la base de datos"))
+                    }
+                })
             }
-        }
-        );
+        })
     }
 }
