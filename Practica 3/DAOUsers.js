bc2ee3c939b426ff53e36e839dfd995052a880ae
SaMuelete01 <105879110+dr4g0nWT@users.noreply.github.com>
2022-10-27 16:53:24 +0200

    update daos

--- Practica 3/DAOUsers.js
+++ Practica 3/DAOUsers.js
@@ -4,4 +4,31 @@ class DAOUsers{
     constructor(pool) {
         this._pool = pool;
     }
+
+    isUserCorrect(email, password, callback) {
+        this.pool.getConnection(function (err, connection) {
+            if (err) {
+                callback(new Error("Error de conexión a la base de datos"));
+            }
+            else {
+                connection.query("SELECT * FROM user WHERE email = ? AND password = ?",
+                    [email, password],
+                    function (err, rows) {
+                        connection.release(); // devolver al pool la conexión
+                        if (err) {
+                            callback(new Error("Error de acceso a la base de datos"));
+                        }
+                        else {
+                            if (rows.length === 0) {
+                                callback(null, false); //no está el usuario con el password proporcionado
+                            }
+                            else {
+                                callback(null, true);
+                            }
+                        }
+                    });
+            }
+        }
+        );
+    }
 }
