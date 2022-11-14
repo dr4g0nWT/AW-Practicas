863d0f62028bed043dbdc7f7caa23391d4558131
SaMuelete01 <samuerodg@gmail.com>
2022-11-14 23:25:49 +0100

    Añadido middleware, ejs de tasks sacando info de la bd e info de la bd actualizada

--- Practica 4/app.js
+++ Practica 4/app.js
@@ -7,12 +7,36 @@ const mysql = require("mysql");
 const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
+const { cachedDataVersionTag } = require("v8");
 // Crear un servidor Express.js
 const app = express();
 // Crear un pool de conexiones a la base de datos de MySQL
 const pool = mysql.createPool(config.mysqlConfig);
 // Crear una instancia de DAOTasks
 const daoT = new DAOTasks(pool);
+//Direccion ficheros estaticos:
+const ficherosEstaticos = path.join(__dirname, "public")
+//Montamos motor de las ejs y ademas añadimos el path views
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+//Middleware static
+app.use(express.static(ficherosEstaticos)) //Lo que hace es permitir buscar la url estatica
+
+//Get para tasks
+app.get("/tasks", function(request, response){
+    //Llamamos a la funcion getAllTasks
+    daoT.getAllTasks("usuario@ucm.es", function(err, result){
+        if(err){
+            console.log(err)        
+        }
+        else{
+            //console.log(result)
+            response.render("tasks", {tasks: result})
+        }
+    })
+})
+
 // Arrancar el servidor
 app.listen(config.port, function (err) {
     if (err) {
